79b78650d5752e94b9391447f139ee1776fdeefa
Limm-jk <201602057@cs-cnu.org>
2020-12-25 21:48:47 +0900

    질문 등록하기

--- pybo/urls.py
+++ pybo/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('question/create/', views.question_create, name='question_create'),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import QuestionForm
 
 
 def index(request):
     return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+
+def question_create(request):
+    """
+    pybo 질문등록
+    """
+    form = QuestionForm()
+    return render(request, 'pybo/question_form.html', {'form': form})
