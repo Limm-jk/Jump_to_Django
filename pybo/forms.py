cfa8f282054a9fae3f037ca042720fc292539106
Limm-jk <201602057@cs-cnu.org>
2020-12-30 01:18:26 +0900

    add : Create question form

--- pybo/forms.py
+++ pybo/forms.py
@@ -3,6 +3,8 @@ from pybo.models import Question
 
 
 class QuestionForm(forms.ModelForm):
+    # 모델 폼은 반드시 가져야함!
+    # 사용할 모델과 필드를 저장
     class Meta:
         model = Question
         fields = ['subject', 'content']
--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.utils import timezone
 
 from .forms import QuestionForm
 from .models import Question
@@ -27,5 +28,17 @@ def question_create(request):
     """
     pybo 질문등록
     """
-    form = QuestionForm()
-    return render(request, 'pybo/question_form.html', {'form': form})
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            
+            return redirect('pybo:index')
+        
+    else:
+        form = QuestionForm()
+        
+    context = {'form' : form}
+    return render(request, 'pybo/question_form.html', context)
