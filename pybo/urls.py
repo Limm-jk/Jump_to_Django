9c2a861f8e3a8d1d75713cf49fa854cbaed08faf
Limm-jk <201602057@cs-cnu.org>
2020-12-30 00:49:19 +0900

    refactor url

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from django.urls.resolvers import URLPattern
 from . import views
 
+# 네임스페이스
+app_name = 'pybo'
+
 urlpatterns = [
     # Config의 url
-    path('', views.index),
+    path('', views.index, name = 'index'),
     # question의 id를 기준으로 조회
-    path('<int:question_id>/', views.detail),
+    path('<int:question_id>/', views.detail, name = 'detail'),
     path('question/create/', views.question_create, name='question_create'),
 ]
